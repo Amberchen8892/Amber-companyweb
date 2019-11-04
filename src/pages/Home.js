14e0f8edbad179fb144802de5ea4d8fb1877a7e9
Phuong Tran <myphuong8892@yahoo.com>
2019-11-04 11:46:17 -0700

    mobile view

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,8 @@ import "react-sweet-progress/lib/style.css";
 
 export default function Home() {
     return (
+      <div>
+        <div id="desktop">
         <div style={{width:"100%", height:"100%", marginTop:"50px"}}>
             <header>
            <Carousel>
@@ -144,6 +146,59 @@ export default function Home() {
       </div>
 </section>
 
+        </div>
+        </div>
+        <div id="mobile">
+        <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="../images/carsbackground2.jpg"
+      alt="First slide"
+      style={{height:"500px"}}
+    />
+    <Carousel.Caption>
+      <h2 class="animated bounceInUp" style={{color:"#0288d1", fontSize:"30px"}}>100 MILION VEHICLES SOLD</h2>
+      <p class="animated bounceInUp delay-1s">MORE THAN 93 MILION CARS SOLD</p>
+      <p class="animated bounceInUp delay-1s">THROUGH OUR CRM</p>
+      <p class="animated bounceInUp delay-1s">AND 7 MILION THROUGH OUR DMS SOFTWARE</p>
+     <div class="animated bounceInUp delays-3s"><Button  variant="info">READ THE PRESS RELEASE <span style={{color:"white"}}><i class="fas fa-arrow-right"></i></span></Button></div>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="../images/shuryl.gif"
+      alt="Third slide"
+      style={{height:"500px"}}
+    />
+
+    <Carousel.Caption style={{marginTop:"50px"}}>
+     <div class="animated bounceInUp delay-1s" style={{backgroundColor:"transparent", width:"250px", textAlign:"center"}}><h2 style={{color:"#0288d1", fontSize:"20px", fontWeight:"bold"}}>CONNECT AND EMPOWER</h2></div> 
+     <div class="animated bounceInUp delay-2s" style={{ backgroundColor:"transparent", width:"250px", textAlign:"center", marginTop:"10px"}}><h3 style={{color:"#f44336", fontsixe:"20px", fontWeight:"bold"}}>YOUR ENTIRE TEAM</h3></div> 
+      <div class="animated bounceInUp delay-3s"><p style={{color:"black", fontSize:"20px"}}>In stead of garbage out, garbage in,</p></div>
+     <div class="animated bounceInUp delay-3s" > <p style={{color:"black", marginTop:"-20px", fontSize:"20px"}}> how's abour good data in, easy wins out?</p></div>
+      <div class="animated bounceInUp delay-4s"><Button variant="info">Watch Now <span style={{color:"white"}}><i class="fas fa-arrow-right"></i></span></Button></div>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="../images/waa2background.jpg"
+      alt="Third slide"
+      style={{height:"500px"}}
+    />
+
+    <Carousel.Caption>
+     <div class="animated bounceInUp " style={{borderBottom:"1px solid", borderColor:"#cfd8dc", width:"400px"}}> <h2 style={{fontSize:"30px", fontWeight:"bold"}}>WE ARE <span style={{fontSize:"30px", fontWeight:"bold", color:"#0288d1"}}>AUTOMOTIVE </span></h2></div>
+     <div  class="animated bounceInUp delay-1s" style={{marginTop:"20px"}}> <p>Exclusive up-to-date market statitics,</p></div>
+     <div  class="animated bounceInUp delay-1s"> <p>you can't get anywhere else</p></div>
+     <div class="animated bounceInUp delay-2s"><Button variant="info">Watch Now <span style={{color:"white"}}><i class="fas fa-arrow-right"></i></span></Button></div>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
+        </div>
         </div>
     )
 }
